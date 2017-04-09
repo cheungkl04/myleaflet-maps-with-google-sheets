642e841a08648d3698d1a276f05e59b7b8498450
Cory <cheungcory@gmail.com>
2017-04-08 20:35:26 -0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1M3hIfFKOKLsWwfBDVe1tJKV206T4TFwqRGI5oHggQpA/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1M3hIfFKOKLsWwfBDVe1tJKV206T4TFwqRGI5oHggQpA';
